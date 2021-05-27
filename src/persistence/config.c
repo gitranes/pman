827e4528015976a529fa0a282427984b092e1c84
salla <rasmus.salla@tuni.fi>
2021-05-27 20:21:47 +0300

    inih hack to make it work :)

--- src/persistence/config.c
+++ src/persistence/config.c
@@ -1,6 +1,15 @@
 #include "persistence/config.h"
 
+#include <ini.h>
+
+#include <stdio.h>
 #include <stdlib.h>
+
+static int config_ini_handler(
+        void* user,
+        const char* section,
+        const char* name,
+        const char* value);
 
 struct ConfigView* config_view_init()
 {
@@ -14,4 +23,14 @@ void config_view_clean(struct ConfigView* view)
         fclose(view->file);
     }
     free(view);
+}
+
+int config_open(struct ConfigView* view, const char* config_path)
+{
+    FILE* ini_fp = NULL;
+    if (!(ini_fp = fopen(config_path, "r")))
+    {
+        return -1;
+    }
+    if
 }
